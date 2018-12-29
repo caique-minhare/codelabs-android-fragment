0f4316d4a9ef5cc5ae5056b5d6646bccf8db8f88
Caique Minhare <caiqueminhare@jera.com.br>
2018-12-28 23:53:36 -0300

    Initial commit

---
+++ FragmentExample1/app/src/main/java/com/example/android/fragmentexample/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.android.fragmentexample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
