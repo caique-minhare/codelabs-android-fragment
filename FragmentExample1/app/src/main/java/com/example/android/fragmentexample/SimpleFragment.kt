15db2b8a94af5d06d58b3ab801e0b1540e9ea171
Caique Minhare <caiqueminhare@jera.com.br>
2018-12-28 23:59:42 -0300

    Created Simple Fragment

---
+++ FragmentExample1/app/src/main/java/com/example/android/fragmentexample/SimpleFragment.kt
@@ -0,0 +1,13 @@
+package com.example.android.fragmentexample
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SimpleFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_simple, container, false)
+    }
+}
