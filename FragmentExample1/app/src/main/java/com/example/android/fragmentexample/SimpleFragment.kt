e679fc7051dc23c5abba3a90c10eb72a3ad3166b
Caique Minhare <caiqueminhare@jera.com.br>
2018-12-29 00:42:29 -0300

    Binding the layout to the Fragment and doing de ui setup

--- FragmentExample1/app/src/main/java/com/example/android/fragmentexample/SimpleFragment.kt
+++ FragmentExample1/app/src/main/java/com/example/android/fragmentexample/SimpleFragment.kt
@@ -1,13 +1,24 @@
 package com.example.android.fragmentexample
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.android.fragmentexample.databinding.FragmentSimpleBinding
+import com.example.android.fragmentexample.utils.OnCheckedChangedListener
 
 class SimpleFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.fragment_simple, container, false)
+    private lateinit var binding: FragmentSimpleBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_simple, container, false)
+        setupUi()
+        return binding.root
+    }
+
+    private fun setupUi(){
+        binding.includedHeader.radioGroup.setOnCheckedChangeListener(OnCheckedChangedListener(binding.includedHeader))
     }
 }
